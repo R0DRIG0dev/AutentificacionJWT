797859d6d7721a13a99edceaa60e13247572b3db
landher <landher__@hotmail.com>
2022-09-17 21:01:18 -0500

    avance hasta el video 8, hay errores si no pones algun parametro cuando insertas customers

--- api.php
+++ api.php
@@ -15,7 +15,7 @@ class Api extends Rest
 		$pass = $this->validarParametro('pass', $this->parametro['pass'], STRING);
 
 		// echo "pasastes";
-		// try {
+		try {
 			$stmt = $this->dbConn->prepare("SELECT * FROM users WHERE email = :email AND password = :pass");
 			$stmt->bindParam(":email", $email);
 			$stmt->bindParam(":pass", $pass);
@@ -41,11 +41,64 @@ class Api extends Rest
 			
 			$data = ['token' => $token];
 			$this->retornarRespuesta(SUCCESS_RESPONSE, $data);
-		// } catch (Exception $e) {
-		// 	$this->throwError(JWT_PROCESSING_ERROR, $e->getMessage());
-		// }
+		} catch (Exception $e) {
+			$this->throwError(JWT_PROCESSING_ERROR, $e->getMessage());
+		}
 	}
 
+	public function agregarCustomer() {
+		$name = $this->validarParametro('name', $this->parametro['name'], STRING, false);
+		$email = $this->validarParametro('email', $this->parametro['email'], STRING, false);
+		$addr = $this->validarParametro('addr', $this->parametro['addr'], STRING, false);
+		$mobile = $this->validarParametro('mobile', $this->parametro['mobile'], INTEGER, false);
+
+		// $cust = new Customer;
+		// $cust->setName($name);
+		// $cust->setEmail($email);
+		// $cust->setAddress($addr);
+		// $cust->setMobile($mobile);
+		// $cust->setCreatedBy($this->userId);
+		// $cust->setCreatedOn(date('Y-m-d'));
+
+		// if(!$cust->insert()) {
+		// 	$message = 'Failed to insert.';
+		// } else {
+		// 	$message = "Inserted successfully.";
+		// }
+
+		// $this->returnResponse(SUCCESS_RESPONSE, $message);
+
+		try {
+			$token = $this->ObtenerTokenPortador();
+			$payload = JWT::decode($token, SECRETE_KEY, ['HS256']);
+			$stmt = $this->dbConn->prepare("SELECT * FROM users WHERE id = :userId");
+			$stmt->bindParam(":userId", $payload->userId);
+			$stmt->execute();
+			$user = $stmt->fetch(PDO::FETCH_ASSOC);
+			if(!is_array($user)) {
+				$this->retornarRespuesta(INVALID_USER_PASS, "Este usuario no se encuentra en nuestra base de datos.");
+			}
+			if( $user['active'] == 0 ) {
+				$this->retornarRespuesta(USER_NOT_ACTIVE, "Este usuario puede estar desactivado. Por favor, póngase en contacto con el administrador.");
+			}
+			$cust = new Customer;
+			$cust->setName($name);
+			$cust->setEmail($email);
+			$cust->setAddress($addr);
+			$cust->setMobile($mobile);
+			$cust->setCreatedBy($payload->userId);
+			$cust->setCreatedOn(date('Y-m-d'));
+			if(!$cust->insert()) {
+				$message = 'Failed to insert.';
+			} else {
+				$message = "Inserted successfully.";
+			}
+			$this->retornarRespuesta(SUCCESS_RESPONSE, $message);	
+			// print_r($payload->userId);
+		} catch (\Exception $e) {
+			$this->lanzarError(ACCESS_TOKEN_ERRORS, $e->getMessage());
+		}
+	}	
 }
 
 ?>
--- rest.php
+++ rest.php
@@ -93,6 +93,41 @@ class Rest
 		$response = json_encode(['resonse' => ['status' => $code, "result" => $data]]);
 		echo $response; exit;
 	}
+
+	/**
+	* Get hearder Authorization
+	* */
+	public function obtenerHeaderAutorizante(){
+		$headers = null;
+		if (isset($_SERVER['Authorization'])) {
+			$headers = trim($_SERVER["Authorization"]);
+		}
+		else if (isset($_SERVER['HTTP_AUTHORIZATION'])) { //Nginx or fast CGI
+			$headers = trim($_SERVER["HTTP_AUTHORIZATION"]);
+		} elseif (function_exists('apache_request_headers')) {
+			$requestHeaders = apache_request_headers();
+			// Server-side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+			$requestHeaders = array_combine(array_map('ucwords', array_keys($requestHeaders)), array_values($requestHeaders));
+			if (isset($requestHeaders['Authorization'])) {
+				$headers = trim($requestHeaders['Authorization']);
+			}
+		}
+		return $headers;
+	}
+	/**
+	 * get access token from header
+	 * */
+	public function ObtenerTokenPortador() {
+		$headers = $this->obtenerHeaderAutorizante();
+		// HEADER: Get the access token from the header
+		if (!empty($headers)) {
+			if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+				return $matches[1];
+			}
+		}
+		$this->throwError( ATHORIZATION_HEADER_NOT_FOUND, 'Access Token Not found');
+	}	
+
 }
 
 ?>
